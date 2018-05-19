3927048e563a5189aa7620ae7e13b1a37a5397ce
Jess Giampietro <giampietrojess@gmail.com>
2018-05-19 00:07:29 -0400

    add mysql database coding

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (burger_name) VALUES ('Lets Give Em Something Shittake Bout Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jingle Bell Pepper Burger');
+INSERT INTO burgers (burger_name) VALUES ('I Know Why the Cajun Burger Sings');
+INSERT INTO burgers (burger_name) VALUES ('Dont Get Creme Fraiche With Me Burger');
+
+
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Southwestern Veggie Burger');
+INSERT INTO menu (item) VALUES ('Spinach Artichoke Turkey Burger');
+INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
+INSERT INTO menu (item) VALUES ('Garden & shroom Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Monster Burger');
