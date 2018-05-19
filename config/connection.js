bd796a2b0b8ba997a90defe186779f83543b0fd4
Jess Giampietro <giampietrojess@gmail.com>
2018-05-19 00:08:17 -0400

    add password due to gitignore not working

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,25 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'VMGJ7B!X',
-    database: 'burgers_db'
-})
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'VMGJ7B!X',
+    database : 'burgers_db'
+  });
+};
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id: " + connection.threadid);
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+
+  console.log('connected as id ' + connection.threadId);
+
 });
 
 module.exports = connection;
